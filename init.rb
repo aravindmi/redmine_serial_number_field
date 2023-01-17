d91b66370e47b1334738ac78b86f6262259aa16f
Aravind MI <aravind.mi@cloud5.com>
2023-01-17 17:49:46 +0530

    updated the version

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :redmine_serial_number_field do
   name 'Redmine Serial Number Field'
   author 'Matsukei Co.,Ltd'
   description 'Add a format to be serial number in the specified format as a issue custom field.'
-  version '3.0.0'
+  version '4.0.0'
   requires_redmine version_or_higher: '4.1'
 end
 
